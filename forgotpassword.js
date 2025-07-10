c6094ef372d686b2355e56dd707c375c38d86353
kryptune <51198180+kryptune@users.noreply.github.com>
2025-07-10 17:52:45 +0800

    add functionalities to forgot password page

---
+++ forgotpassword.js
@@ -0,0 +1,32 @@
+function login() {
+windows.location.href = "https://blckjck2.netlify.app";
+}
+
+function register() {
+  windows.location.href = "https://blckjck2.netlify.app";
+  createAccountForm();
+}
+
+function reset() {
+  const email = document.getElementById("email-el").value;
+  if (email) {
+    alert("A password reset link has been sent to " + email);
+    fetch("${API_BASE}/send-email", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        to: email,
+        subject: "Password Reset Request",
+        message:
+          "Please click the link below to reset your password:\n\n" +
+          "https://yourwebsite.com/reset-password?email=" +
+          encodeURIComponent(email),
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  } else {
+    alert("Email is required for password reset.");
+  }
+}
